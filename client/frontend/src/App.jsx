0774bb472e317e12610fd9ab03105918563f6f9f
SanjanaNilanka <nilankasanjana803@gmail.com>
2025-01-11 17:41:29 +0530

    intigrate MUI

---
+++ client/frontend/src/App.jsx
@@ -0,0 +1,89 @@
+import * as React from 'react';
+import {useEffect, useState} from 'react';
+import Box from '@mui/material/Box';
+import { ThemeProvider, useTheme, createTheme } from '@mui/material/styles';
+import { amber, deepOrange, grey } from '@mui/material/colors';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { Button, CssBaseline } from '@mui/material';
+import customTheme from './customTheme';
+//import AppAppBar from './components/appbar/AppAppBar';
+//import Footer from './components/footer/Footer';
+
+import Login from './Components/Login';
+import Signup from './Components/Signup';
+import Home from './Components/Home';
+import WelcomePage from './Components/WelcomePage';
+import RefrshHandler from './RefrshHandler';
+import InsuranceClaim from './Components/InsuranceClaim'; 
+import PayInsurance from './Components/PayInsurance'; 
+import CompanyDetailsPage from './Components/CompanyDetailsPage';
+import PaymentSuccess from './Components/PaymentSuccess';
+import PaymentFailure from './Components/PaymentFailure';
+import Footer from './Components/Footer';
+import AppAppBar from './Components/AppAppBar';
+
+
+const App = () => {
+  const [themeMode, setThemeMode] = React.useState('light')
+  const currentTheme = createTheme(customTheme(themeMode),);
+
+  useEffect(() => {
+    const themeFromLocalStorage = localStorage.getItem('theme');
+    if (themeFromLocalStorage) {
+      setThemeMode(themeFromLocalStorage);
+    } else {
+      setThemeMode('light');
+    }
+  }, []);
+  
+  const theme = useTheme();
+  const toogleTheme = () => {
+    if (themeMode === 'light') {
+      setThemeMode('dark')
+      localStorage.setItem('theme', 'dark');
+    } else {
+      setThemeMode('light')
+      localStorage.setItem('theme', 'light');
+    }
+    
+  }
+  
+  const [is404, setIs404] = React.useState(false);
+  const [isNavbarHidden, setIsNavbarHidden] = React.useState(false);
+  const [isFooterHiddden, setIsFooterHidden] = React.useState(false);
+  const currentLocation = window.location.pathname;
+  
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  
+  const PrivateRoute = ({ element }) => {
+    return isAuthenticated ? element : <Navigate to="/login" />;
+  };
+
+  return (
+    <ThemeProvider theme={currentTheme}>
+      <CssBaseline />
+      <RefrshHandler setIsAuthenticated={setIsAuthenticated} />
+      <header>
+        <AppAppBar toggleColorMode={toogleTheme} mode={themeMode} />
+      </header>
+      <main>
+          <Routes>
+            <Route path="/" element={<WelcomePage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/home" element={<PrivateRoute element={<Home />} />} />
+            <Route path="/pay-insurance" element={<PayInsurance />} />
+            <Route path="/insurance-claim" element={<InsuranceClaim />} />
+            <Route path="/company/:companyName" element={<CompanyDetailsPage />} />
+            <Route path="/success" element={<PaymentSuccess />} />
+            <Route path="/cancel" element={<PaymentFailure />} />
+          </Routes>
+      </main>
+      <footer>
+        <Footer/>
+      </footer>
+    </ThemeProvider>
+  );
+}
+
+export default App;
--- client/frontend/src/Components/WelcomePage.js
+++ client/frontend/src/Components/WelcomePage.js
@@ -4,7 +4,10 @@ import Footer from './Footer';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper/modules';
 import { Link } from 'react-router-dom'; // Add this import
-
+import Paper from '@mui/material/Paper'
+import Card from '@mui/material/Card'
+import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
 
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -27,7 +30,8 @@ function WelcomePage() {
 
   return (
     <div className="welcome-container">
-      <header className="navbar">
+
+      {/*<header className="navbar">
         <h1>Vehicle Insurance Co.</h1>
         <ul className="nav-links">
           <li><Link to="/">Home</Link></li>
@@ -41,71 +45,68 @@ function WelcomePage() {
             <button className="signup-button">Signup</button>
           </Link>
         </ul>
-      </header>
-      
-      <main>
-        <div className='welcome-text'>
-          <h1>Welcome to Vehicle Insurance Co.</h1>
-          <p>Your trusted partner for comprehensive vehicle insurance plans.</p>
-        </div>
+      </header>*/}
 
-        <section className='plans-section'>
-          <h2>Choose your Insurance Partner</h2>
-          <Swiper
-            className='swiper'
-            loop={true}
-            autoplay={{
-              delay: 1500,
-              disableOnInteraction: false,
-              pauseOnMouseEnter: true
-            }}
-            modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
-            spaceBetween={0}
-            breakpoints={{
-              320: { slidesPerView: 1 },
-              410: { slidesPerView: 2 },
-              734: { slidesPerView: 3 },
-              1100: { slidesPerView: 4 },
-            }}
-            navigation
-          >
-            {/* SwiperSlide items */}
-            {[
-              { name: 'Company 1', image: Company1 },
-              { name: 'Company 2', image: Company2 },
-              { name: 'Company 3', image: Company3 },
-              { name: 'Company 4', image: Company4 },
-              { name: 'Company 5', image: Company5 }
-            ].map((company, index) => (
-              <SwiperSlide key={index}>
-                <div className='slide-container'>
-                  <img src={company.image} alt={company.name} />
-                  <h3>{company.name}</h3>
-                  <p>{company.name} offers reliable and customizable insurance plans.</p>
-                  <button onClick={() => handleViewMore(company.name)}>View More</button>
-                </div>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </section>
+      <Box sx={{mt: 15}}/>
+      <div className='welcome-text'>
+        <h1>Welcome to Vehicle Insurance Co.</h1>
+        <p>Your trusted partner for comprehensive vehicle insurance plans.</p>
+      </div>
 
-        <section className="features-section">
-          <div className="feature">
-            <h3>Comprehensive Coverage</h3>
-            <p>Covering accidents, theft, and more to keep you protected.</p>
-          </div>
-          <div className="feature">
-            <h3>24/7 Customer Support</h3>
-            <p>Always here to help you with claims and questions.</p>
-          </div>
-          <div className="feature">
-            <h3>Easy Claims Process</h3>
-            <p>Hassle-free claims to get you back on the road quickly.</p>
-          </div>
-        </section>
-      </main>
+      <section className='plans-section'>
+        <h2>Choose your Insurance Partner</h2>
+        <Swiper
+          className='swiper'
+          loop={true}
+          autoplay={{
+            delay: 1500,
+            disableOnInteraction: false,
+            pauseOnMouseEnter: true
+          }}
+          modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
+          spaceBetween={0}
+          breakpoints={{
+            320: { slidesPerView: 1 },
+            410: { slidesPerView: 2 },
+            734: { slidesPerView: 3 },
+            1100: { slidesPerView: 4 },
+          }}
+          navigation
+        >
+          {/* SwiperSlide items */}
+          {[
+            { name: 'Company 1', image: Company1 },
+            { name: 'Company 2', image: Company2 },
+            { name: 'Company 3', image: Company3 },
+            { name: 'Company 4', image: Company4 },
+            { name: 'Company 5', image: Company5 }
+          ].map((company, index) => (
+            <SwiperSlide key={index}>
+              <div className='slide-container'>
+                <img src={company.image} alt={company.name} />
+                <h3>{company.name}</h3>
+                <p>{company.name} offers reliable and customizable insurance plans.</p>
+                <button onClick={() => handleViewMore(company.name)}>View More</button>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </section>
 
-      <Footer />
+      <Card elevation={3} className="features-section" sx={{px:10, gap: 10, borderRadius: 0}}>
+        <Paper sx={{px:3, py: 5, display: 'flex', flexDirection: 'column', gap: 2}}>
+          <Typography variant='h5' sx={{color:'#2a9d8f'}}>Comprehensive Coverage</Typography>
+          <Typography variant='body1'>Covering accidents, theft, and more to keep you protected.</Typography>
+        </Paper>
+        <Paper sx={{px:3, py: 5, display: 'flex', flexDirection: 'column', gap: 2}}>
+          <Typography variant='h5' sx={{color:'#2a9d8f'}}>24/7 Customer Support</Typography>
+          <Typography variant='body1'>Always here to help you with claims and questions.</Typography>
+        </Paper>
+        <Paper sx={{px:3, py: 5, display: 'flex', flexDirection: 'column', gap: 2}}>
+          <Typography variant='h5' sx={{color:'#2a9d8f'}}>Easy Claims Process</Typography>
+          <Typography variant='body1'>Hassle-free claims to get you back on the road quickly.</Typography>
+        </Paper>
+      </Card>
     </div>
   );
 }
